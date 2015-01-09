be703b262c6f3671b8d0fff00d3bbfa50f529da1
Rahul Horé <hore.rahul@gmail.com>
2015-01-09 16:45:18 -0500

    Adds postgreql dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
 (defproject bloj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "bloj — a simple blog in Clojure"
+  :url "https://github.com/O-I/bloj"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]]
+                 [ring/ring-defaults "0.1.2"]
+                 [org.clojure/java.jdbc "0.3.5"]
+                 [postgresql "9.1-901.jdbc4"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler bloj.core.handler/app}
   :profiles
