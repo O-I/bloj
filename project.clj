a8aeee7faace0c69d0d70994e6d014c4c5fc7cd3
Rahul Horé <hore.rahul@gmail.com>
2015-01-09 19:41:45 -0500

    Adds a main- form
    I have no idea what this actually does. Maybe this is needed to get the app to run on Heroku? I'm switching between the Heroku Shouter tutorial (uses an older version of Compojure but has PostgreSQL setup and cleaner code) and Mark Gandolfo's blog tutorial (newer version of Compojure, but the code isn't very DRY and he uses MySQL for persistence) to try to get the bits I want, but I don't know enough yet to know if I'm duplicating things unnecessarily. Just trying to get this to work for now.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
+                 [ring/ring-jetty-adapter "1.2.1"]
                  [org.clojure/java.jdbc "0.3.5"]
                  [postgresql "9.1-901.jdbc4"]
                  [de.ubercode.clostache/clostache "1.3.1"]]
--- src/bloj/core/handler.clj
+++ src/bloj/core/handler.clj
@@ -2,11 +2,15 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [bloj.controllers.posts :as posts-controller]))
+            [ring.adapter.jetty :as ring]
+            [bloj.controllers.posts :as posts]))
 
 (defroutes app-routes
-  (GET "/" [] (posts-controller/index))
+  (GET "/" [] (posts/index))
   (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+(defn -main []
+  (ring/run-jetty #'app-routes {:port 8080 :join? false}))
