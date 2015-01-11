68bcc467bc6afbb7b66a2a86728a636f544e29a6
Rahul Horé <hore.rahul@gmail.com>
2015-01-10 19:02:12 -0500

    Updates handler.clj à la shouter demo app

--- src/bloj/core/handler.clj
+++ src/bloj/core/handler.clj
@@ -3,14 +3,22 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.adapter.jetty :as ring]
-            [bloj.controllers.posts :as posts]))
+            [bloj.controllers.posts :as posts]
+            [bloj.views.layout :as layout]
+            [bloj.models.migration :as schema]))
 
 (defroutes app-routes
-  (GET "/" [] (posts/index))
-  (route/not-found "Not Found"))
+  posts/routes
+  (route/resources "/")
+  (route/not-found (layout/four-oh-four)))
 
 (def app
   (wrap-defaults app-routes site-defaults))
 
+(defn start [port]
+  ring/run-jetty app {:port port :join? false})
+
 (defn -main []
-  (ring/run-jetty #'app-routes {:port 8080 :join? false}))
+  (schema/migrate)
+  (let [port (Integer. (or (System/getenv "PORT") "8080"))]
+    (start port)))
