54d102129fbc65b536dd405e396f29e4d10ef391
Rahul Horé <hore.rahul@gmail.com>
2015-01-11 22:31:18 -0500

    Updates project dependencies
    I'm having trouble getting the app to post without running into an invalid anti-forgery token error. I'm mimicking the shouter app's dependencies to get something working.

--- project.clj
+++ project.clj
@@ -1,16 +1,13 @@
-(defproject bloj "0.1.0-SNAPSHOT"
+(defproject bloj "0.0.1"
   :description "bloj — a simple blog in Clojure"
   :url "https://github.com/O-I/bloj"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]
                  [ring/ring-jetty-adapter "1.2.1"]
                  [org.clojure/java.jdbc "0.3.5"]
                  [postgresql "9.1-901.jdbc4"]
                  [hiccup "1.0.4"]]
-  :plugins [[lein-ring "0.8.13"]]
-  :ring {:handler bloj.core.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+  :main ^:skip-aot bloj.web
+  :uberjar-name "bloj-standalone.jar"
+  :profiles {:uberjar {:aot :all}})
