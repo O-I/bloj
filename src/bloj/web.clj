8467ec15842692e71579f5b431faf522f9366818
Rahul Horé <hore.rahul@gmail.com>
2015-01-26 19:57:50 -0500

    Adds basic authentication machinery
    This is from following this [Friend tutorial](https://github.com/ddellacosta/friend-interactive-form-tutorial) verbatim. Trying to figure out how it works.

--- src/bloj/web.clj
+++ src/bloj/web.clj
@@ -10,15 +10,34 @@
             (cemerick.friend [workflows :as workflows]
                              [credentials :as creds]))
   (:gen-class))
-  
+
+(def users {"admin" {:username "admin"
+                    :password (creds/hash-bcrypt "password")
+                    :roles #{::admin}}
+            "rahul" {:username "rahul"
+                    :password (creds/hash-bcrypt "password")
+                    :roles #{::user}}})
+
+(derive ::admin ::user)
 
 (defroutes app-routes
   posts/routes
   (route/resources "/")
+  (GET "/authorized" request
+       (friend/authorize #{::user} "This page can only be seen by authenticated users."))
+  (GET "/admin" request
+       (friend/authorize #{::admin} "This page can only be seen by administrators."))
+  (GET "/login" [] (-> "login.html"
+                       (ring.util.response/file-response {:root "resources"})
+                       (ring.util.response/content-type "text/html")))
+  (friend/logout (ANY "/logout" request (ring.util.response/redirect "/")))
   (route/not-found (layout/four-oh-four)))
 
 (def app
-  (handler/site app-routes))
+  (handler/site
+    (friend/authenticate app-routes
+        {:credential-fn (partial creds/bcrypt-credential-fn users)
+                         :workflows [(workflows/interactive-form)]})))
 
 (defn start [port]
   (ring/run-jetty app {:port port :join? false}))
